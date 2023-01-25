32e89018fb6eea403d803cd6b2548ff3f6176999
harksu <harksulim@naver.com>
2023-01-26 01:26:24 +0900

    5장 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [PostsModule, UsersModule],
+  imports: [PostsModule, UsersModule, EmailModule],
   controllers: [],
   providers: [],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { EmailService } from 'src/email/email.service';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  providers: [UsersService],
 })
 export class UsersModule {}
