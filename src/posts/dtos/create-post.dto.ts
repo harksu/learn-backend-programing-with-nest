1c661c0405174f0d38e5b5fade59cc633070f7db
harksu <harksulim@naver.com>
2023-02-01 01:23:33 +0900

    feat: dto validation 추가

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,5 +1,19 @@
+import { IsString, MinLength, MaxLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+
 export class createPostDto {
+  @Transform((params) => params.value.trim())
+  @IsString()
   readonly id: string;
+
+  @Transform((params) => params.value.trim())
+  @IsString()
+  @MinLength(2)
+  @MaxLength(20)
   readonly title: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(500)
   readonly content: string;
 }
